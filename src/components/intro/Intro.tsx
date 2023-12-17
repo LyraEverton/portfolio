0a2abf92ff86b5c68eee8ff45a4f1f788fe9a76d
Everton Lyra <everton@evertonlyra.com>
2023-12-17 19:46:32 +0000

    Fixed responsivity of Intro and Navbar, starting to work on LatestProjects

--- src/components/intro/Intro.tsx
+++ src/components/intro/Intro.tsx
@@ -14,26 +14,33 @@ export const Intro = () => {
   </span>
     )
     return (
-        <div className={"introduction py-25 px-15"}>
-            <div className={"introduction__name"}>
-                <span>Hello</span>
-                <Emoji label="sheep" symbol="👋"/>
-                <span className={"d-block"}>I´m Everton</span>
-            </div>
-            <div className={"introduction__aboutMe mt-1"}>
-                <span>A Portugal based frontend developer, working and living in OPorto. I love to work on anything code, from designing simple HTML pages to solving complex problems.</span>
-                <div className={"introduction__aboutMe__socials mt-2"}>
-                    <a href={"/"}
-                       className={"introduction__aboutMe__socials__twitter"}>{i18next.t('ns1:navbar.twitter')}</a>
-                    <a href={"/"}
-                       className={"introduction__aboutMe__socials__linkedin"}>{i18next.t('ns1:navbar.linkedin')}</a>
-                    <a href={"/"}
-                       className={"introduction__aboutMe__socials__facebook"}>{i18next.t('ns1:navbar.facebook')}</a>
+        <div className={"introduction pt-25 px-15 pt-lg-16"}>
+            <div className={"d-md-flex"}>
+                <div className={"introduction__name pl-md-3 d-md-flex items-center"}>
+                    <div className={"introduction__name__div"}>
+                        <span>Hello</span>
+                        <Emoji label="sheep" symbol="👋"/>
+                        <span className={"d-block"}>I´m Everton</span>
+                    </div>
+                </div>
+                <hr className={"introduction__verticalLine d-none d-md-block mx-md-3"}/>
+                <div className={"introduction__aboutMe mt-1"}>
+                    <span className={"d-flex pr-3"}>A Portugal based frontend developer, working and living in OPorto. I love to work on anything code, from designing simple HTML pages to solving complex problems.</span>
+                    <div className={"introduction__aboutMe__socials mt-2"}>
+                        <a href={"/"}
+                           className={"introduction__aboutMe__socials__twitter"}>{i18next.t('ns1:navbar.twitter')}</a>
+                        <a href={"/"}
+                           className={"introduction__aboutMe__socials__linkedin"}>{i18next.t('ns1:navbar.linkedin')}</a>
+                        <a href={"/"}
+                           className={"introduction__aboutMe__socials__facebook"}>{i18next.t('ns1:navbar.facebook')}</a>
+                    </div>
                 </div>
             </div>
-            <div className={"introduction__scrollProjects d-flex justify-items-center mt-4"}>
+
+            <div className={"introduction__scrollProjects d-block justify-center items-start"}>
                 <div className="introduction__scrollProjects__longArrow"></div>
             </div>
+
         </div>
     )
 }
--- src/components/latestProjects/LatestProjects.tsx
+++ src/components/latestProjects/LatestProjects.tsx
@@ -2,7 +2,9 @@ import "./LatestProjects.scss";
 export const LatestProjects = () => {
     return (
         <div className={"latestProjects"}>
+            <div className={"latestProjects__content"}>
 
+            </div>
         </div>
     )
 }
